1a30adfc28b7b9c740a9ce4c58f3bb5dd4ad73e1
user <nightmareeye>
2022-11-24 22:32:19 +0300

    fixed database

--- app/database.py
+++ app/database.py
@@ -1,12 +1,14 @@
 """Database for todo
 """
+import os.path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session  #pylint: disable=unused-import
 
 from models import Base
 
-DB_URL = "sqlite:///./db.sqlite"
+DB_PATH ="data"
+DB_URL = os.path.join("sqlite:///", os.path.join(DB_PATH, "db.sqlite"))
 ENGINE = create_engine(DB_URL, connect_args={"check_same_thread": False})
 # for logging all SQL-queries
 #ENGINE = create_engine(DB_URL, connect_args={"check_same_thread": False}, echo=True)
@@ -15,6 +17,8 @@ SESSIONLOCAL = sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
 def init_db():
     """Init database, create all models as tables
     """
+    if not os.path.exists(DB_PATH):
+        os.mkdir(DB_PATH)
     # check_same_thread is for SQLite only
     Base.metadata.create_all(bind=ENGINE)
 
